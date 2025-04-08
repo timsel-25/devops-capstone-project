78c1758ad34c1cc87f4075f35b7a8bf1d0a15ad2
timsel-25 <arvai.hajnalka@rubin.hu>
2025-04-08 11:28:15 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -12,6 +12,7 @@ from . import app  # Import Flask application
 ############################################################
 # Health Endpoint
 ############################################################
+
 @app.route("/health")
 def health():
     """Health Status"""
@@ -20,6 +21,7 @@ def health():
 ######################################################################
 # GET INDEX
 ######################################################################
+
 @app.route("/")
 def index():
     """Root URL response"""
@@ -35,6 +37,7 @@ def index():
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
@@ -57,6 +60,7 @@ def create_accounts():
     ######################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -74,6 +78,7 @@ def list_accounts():
     ######################################################################
     # READ AN ACCOUNT
     ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -91,6 +96,7 @@ def get_accounts(account_id):
     ######################################################################
     # UPDATE AN EXISTING ACCOUNT
     ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
